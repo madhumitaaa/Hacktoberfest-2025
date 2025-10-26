cadbe456ceec40d3dc8ebda204de4e3d6ace7adb
madhumitaaa <madhumita.mandal.ee26@heritageit.edu.in>
2025-10-27 03:38:16 +0530

    added BFS of a Graph

--- C++/BFSofaGraph.cpp
+++ C++/BFSofaGraph.cpp
@@ -31,45 +31,21 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    // Example usage:
-    // n = number of vertices (0..n-1), m = number of edges
-    // Input format example:
-    // 5 6
-    // 0 1
-    // 0 2
-    // 1 2
-    // 1 3
-    // 2 3
-    // 3 4
-    // src = 0
-
     int n, m;
-    if (!(cin >> n >> m)) {
-        // Fallback demo graph if no input provided
-        n = 5; m = 6;
-        vector<pair<int,int>> edges = {
-            {0,1},{0,2},{1,2},{1,3},{2,3},{3,4}
-        };
-        vector<vector<int>> adj(n);
-        for (auto [u,v] : edges) {
-            adj[u].push_back(v);
-            adj[v].push_back(u); // comment out for directed graph
-        }
-        int src = 0;
-        auto order = bfs(n, src, adj);
-        for (int i = 0; i < order.size(); ++i) cout << order[i] << (i+1==order.size()?'\n':' ');
-        return 0;
-    }
-
+    cin >> n >> m; // number of vertices and edges
     vector<vector<int>> adj(n);
     for (int i = 0; i < m; ++i) {
         int u, v; cin >> u >> v;
         adj[u].push_back(v);
-        adj[v].push_back(u); // comment out for directed graph
+        adj[v].push_back(u); // remove this line for directed graph
     }
-    int src; cin >> src;
+
+    int src; cin >> src; // starting vertex for BFS
     auto order = bfs(n, src, adj);
-    for (int i = 0; i < order.size(); ++i) cout << order[i] << (i+1==order.size()?'\n':' ');
+
+    // Print BFS traversal
+    for (int i = 0; i < order.size(); ++i) 
+        cout << order[i] << (i+1==order.size()?'\n':' ');
 
     return 0;
 }
